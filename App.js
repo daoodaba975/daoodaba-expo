8b46e77ac90f36f7481a4a65e85c3b91104b6c3b
daooda <davinhoba9@outlook.fr>
2020-05-19 01:00:46 +0000

    UPDATE.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Daouda BA</Text>
     </View>
   );
 }
@@ -16,4 +16,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "crimson",
+  }
 });
