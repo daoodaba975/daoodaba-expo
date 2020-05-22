cfc443872641c5240c804fe1b340e9de656ce5f7
daooda <davinhoba9@outlook.fr>
2020-05-22 01:49:32 +0000

    update.

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
+
+import Avatar from './components/avatar';
+import About from './components/about';
+import Social from './components/social';
+import CallButton from './components/callbutton';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'adventpro-bold': require('./assets/fonts/AdventPro-Bold.ttf'),
+    'adventpro-regular': require('./assets/fonts/AdventPro-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Daouda BA</Text>
+      <Avatar />
+      <About />
+      <Social />
+      <CallButton />
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202c39',
     alignItems: 'center',
     justifyContent: 'center',
   },
